50cce234f91bc0a64f22faa8ebc63b61398ba585
emrep <emrewindowsarg@gmail.com>
2022-07-14 13:00:36 +0300

    Initial commit

---
+++ app/src/main/java/com/emrepbu/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.emrepbu.weatherapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import com.emrepbu.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                Text(text = "asd")
+            }
+        }
+    }
+}
